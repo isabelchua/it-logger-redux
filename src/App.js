6b3953b33b5b93e58bc78a85b7dd04a106a0771a
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-14 18:45:15 -0700

    finished ui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 
 function App() {
 	useEffect(() => {
@@ -16,7 +17,7 @@ function App() {
 			<SearchBar />
 			<div className="container">
 				<AddBtn />
-
+				<AddLogModal />
 				<Logs />
 			</div>
 		</Fragment>
